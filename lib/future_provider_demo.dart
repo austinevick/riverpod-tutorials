55c4eb858da8f9c8d181df16bf8a5b71e30bc9b3
Victor Augustine <augustinevickky@gmail.com>
2022-10-27 12:45:48 +0100

    first commit

---
+++ lib/future_provider_demo.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum City {
+  lagos,
+  oyo,
+  akure,
+}
+
+typedef WeatherEmoji = String;
+Future<WeatherEmoji> getWeather(City city) {
+  return Future.delayed(const Duration(seconds: 2),
+      () => {City.lagos: "❄", City.akure: "🌧", City.oyo: "☀"}[city]!);
+}
+
+final currentCityWeather = StateProvider<City?>((ref) => null);
+final weatherFutureProvider = FutureProvider<String>((ref) {
+  final city = ref.watch(currentCityWeather);
+  if (city != null) {
+    return getWeather(city);
+  }
+  return '';
+});
+
+class FutureProviderDemo extends ConsumerWidget {
+  const FutureProviderDemo({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(height: 35),
+          ref.watch(weatherFutureProvider).when(
+              data: (data) => Center(
+                    child: Text(data),
+                  ),
+              error: (e, t) => Container(),
+              loading: () => const Center(
+                    child: CircularProgressIndicator(),
+                  )),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: City.values.length,
+                  itemBuilder: (ctx, i) {
+                    final city = City.values[i];
+                    final isSelected = city == ref.watch(currentCityWeather);
+                    return ListTile(
+                      title: Text(city.name),
+                      trailing: isSelected ? const Icon(Icons.check) : null,
+                      onTap: () =>
+                          ref.read(currentCityWeather.notifier).state = city,
+                    );
+                  }))
+        ],
+      ),
+    );
+  }
+}
